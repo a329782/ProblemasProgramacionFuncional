a4a82ab8d61a974efa4c156bc4734666fc7e3d0d
a329782 <a329782@uach.mx>
2021-04-29 00:05:34 -0600

    Cambios en el texto para que se vea bonito, y agregue unos pocos mas productos

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ class tienditaDAO{
     }
 
     entrePrecio(n1, n2){
-        return this.productsList.filter(products => products.price > n1 && product.price < n2);
+        return this.productsList.filter(products => products.price > n1 && products.price < n2);
     }
 
     mismaClasificacion(category){
@@ -43,12 +43,15 @@ class tienditaDAO{
 
 var productsList = [];
 
+//Y asi podriamos seguir agregando mas productos al carrito y funcionaria
 productsList.push(new Products(1,"Cilantro", 5.99, "Verdura", 50, 39, 160));
 productsList.push(new Products(2, "Platano Chiapas", 5.90, "Fruta", 24, 10, 50));
 productsList.push(new Products(3,"Cebolla", 5.99, "Verdura", 12, 9, 20));
 productsList.push(new Products(4, "Chile Chilaca", 8.80, "Verdura", 18, 10, 40));
 productsList.push(new Products(5,"Patata", 2.90, "Verdura", 8, 5, 12));
 productsList.push(new Products(6, "Lechuga", 4.78, "Verdura", 9, 6, 15));
+productsList.push(new Products(7, "Melon", 27.80, "Fruta", 12, 10, 50));
+productsList.push(new Products(8, "Tomate", 15.80, "Verdura", 19, 12, 40));
 
 let elProducto = new tienditaDAO(productsList);
 
@@ -59,17 +62,17 @@ let elProducto = new tienditaDAO(productsList);
 */
 
 
-console.log("1. El numero de productos con existencia mayor a 20 es: ");
+console.log("1. Los productos con existencia mayor a 20 son: ");
 console.log(elProducto.existenciaMayor(20));
 
-console.log("2. El numero de productos con existencia menor a 15 es: ");
+console.log("2. Los productos con existencia menor a 15 son: ");
 console.log(elProducto.existenciaMenor(15));
 
-console.log("3. La lista de productos con el precio mayor a 15.50 y la misma clasificacion es: ");
+console.log("3. La lista de productos con el precio mayor a 15.50 y la misma clasificacion son: ");
 console.log(elProducto.mismaClasificacionPrecio("Verdura", 15.50));
 
-console.log("4. Las lista de productos con el precio mayor a 20.30 y menor a 45 es: ");
+console.log("4. Las lista de productos con el precio mayor a 20.30 y menor a 45 son: ");
 console.log(elProducto.entrePrecio(20.30, 45.00));
 
-console.log("5. El numero de productos agrupados por su clasificacion es: ");
+console.log("5. Los productos agrupados por su clasificacion son: ");
 console.log(elProducto.mismaClasificacion("Verdura"));
