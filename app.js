02a2e316a94abfcfa54d2785ce677317e67b2506
a329782 <a329782@uach.mx>
2021-04-28 23:43:19 -0600

    Se agregan las funciones

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-class ProductsDAO
+class Products
 {
     constructor(id, name, price, category, stock, minStock, maxStock)
     {
@@ -10,7 +10,37 @@ class ProductsDAO
         this.minStock = minStock;
         this.maxStock = maxStock;
     }
+
 }
+
+class tienditaDAO{
+    constructor (productsList){
+        this.productsList = productsList;
+    }
+
+    existenciaMayor(value){
+        return this.productsList.filter(products => products.stock > value);
+    }
+
+    existenciaMenor(value){
+        return this.productsList.filter(products => products.stock < value);
+    }
+
+    mismaClasificacionPrecio(category, price){
+        return this.productsList.filter(products => products.price > price && products.category === category);
+    }
+
+    entrePrecio(n1, n2){
+        return this.productsList.filter(products => products.price > n1 && product.price < n2);
+    }
+
+    mismaClasificacion(category){
+        return this.productsList.filter(products => products.category === category);
+    }
+
+
+}
+
 
 var product1 = new ProductsDAO(1,"Cilantro", 5.99, "Verdura", 50, 39, 160);
 var product2 = new ProductsDAO(2, "Juan", 12, "Persona", 25, 10, 40);
